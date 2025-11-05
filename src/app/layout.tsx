c56bab5e725e6ea5d7089aea3717dd6e78d8a276
SAATH Academy Samundri <saathacademysamundri@gmail.com>
2025-11-05 05:44:31 +0000

    Install PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { SettingsProvider } from '@/hooks/use-settings';
 export const metadata: Metadata = {
   title: 'My Academy',
   description: 'An Academy Management System',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -21,6 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
