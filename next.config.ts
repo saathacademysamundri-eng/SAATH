880dacd6b5ae27c47cb1e701bee58e4138cd0995
SAATH Academy Samundri <saathacademysamundri@gmail.com>
2025-11-07 04:38:35 +0000

    I want to add such an installation page to my website. Please add it. If

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 import withPWA from '@ducanh2912/next-pwa';
 
@@ -43,7 +44,7 @@ const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development' ? false : true,
 });
 
 export default pwaConfig(nextConfig);
