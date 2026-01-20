c51f06fdcb4f299f2bf28a52b08936a3909295b0
SAATH Academy Samundri <saathacademysamundri@gmail.com>
2026-01-20 10:56:08 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/(app)/exams/page.tsx
+++ src/app/(app)/exams/page.tsx
@@ -31,6 +31,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { useToast } from '@/hooks/use-toast';
 import { EditExamDialog } from './edit-exam-dialog';
